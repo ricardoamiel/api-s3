ea5951b006c796ec6cc3178c499fb0906dd5ed40
ricardoamiel <109002818+ricardoamiel@users.noreply.github.com>
2024-07-03 08:38:28 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -6,7 +6,12 @@ def lambda_handler(event, context):
     
     # Proceso    
     s3 = boto3.client('s3')
-    response = s3.create_bucket(Bucket=nombre_bucket)
+    response = s3.create_bucket(
+        Bucket=nombre_bucket,
+        CreateBucketConfiguration={
+            'LocationConstraint': 'us-east-1'
+        }
+    )
 
     return {
         'statusCode': 200,
