f1521e5ef5a7493b063a48397150810a13e19fd4
ricardoamiel <109002818+ricardoamiel@users.noreply.github.com>
2024-07-03 08:44:17 -0500

    Update crear_bucket.py aaaa

--- crear_bucket.py
+++ crear_bucket.py
@@ -3,15 +3,19 @@ import boto3
 def lambda_handler(event, context):
     # Entrada (json)
     nombre_bucket = event['body']['bucket']
+    region = 'us-east-1'
     
     # Proceso    
-    s3 = boto3.client('s3')
-    response = s3.create_bucket(
-        Bucket=nombre_bucket,
-        CreateBucketConfiguration={
-            'LocationConstraint': 'us-east-1'
-        }
-    )
+    s3 = boto3.client('s3', region_name=region)
+    if region == 'us-east-1':
+        response = s3.create_bucket(Bucket=nombre_bucket)
+    else:
+        response = s3.create_bucket(
+            Bucket=nombre_bucket,
+            CreateBucketConfiguration={
+                'LocationConstraint': region
+            }
+        )
 
     return {
         'statusCode': 200,
